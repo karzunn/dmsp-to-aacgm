82efa8fb20b9d1319a39e423f7d583960b09efa1
Carson O'Ffill <karzunn@users.noreply.github.com>
2025-01-20 21:55:31 -0500

    wip

--- dmsp_to_aacgm.py
+++ dmsp_to_aacgm.py
@@ -1,5 +1,43 @@
+from typing import Callable
 import click
+import h5py
+from abc import ABC, abstractmethod
 
-@click.group()
-def cli():
-    pass
+class DataSet(ABC):
+
+    @abstractmethod
+    def convert(self): ...
+
+    @abstractmethod
+    def save(self, output_path: str): ...
+
+
+def data_reader_factory(file_path: str) -> Callable:
+    if file_path.endswith(".hdf5"):
+        return hdf5_reader
+    raise Exception(f"Cannot process file type: {file_path.split(".")[-1]}")
+    
+def hdf5_reader(file_path: str) -> DataSet:
+    contents = h5py.File(file_path, "r")
+    if contents.get("Data", {}).get("Table Layout"):
+        return DmspHdf5Data(file_path)
+
+
+class DmspHdf5Data(DataSet):
+    def __init__(self, path: str):
+        self.contents = h5py.File(path, "r")
+        self.data_set = self.contents["Data"]["Table Layout"]
+        print(self.data_set.dtype.descr)
+
+    def convert(self): ...
+
+    def save(self): ...
+
+
+@click.command()
+@click.argument("path", type=click.Path(exists=True))
+def cli(path):
+    file_reader = data_reader_factory(path)
+    data_set = file_reader(path)
+    data_set.convert()
+    data_set.save()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
+    requirements = fh.read().splitlines()
 
 setup(
     name = 'dmsp-to-aacgm',
