8d88110ef6db6c92354b68bb69f755326fa02ea9
Carson O'Ffill <karzunn@users.noreply.github.com>
2025-01-21 14:50:15 -0500

    version change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'dmsp-to-aacgm',
-    version = '0.0.1',
+    version = '0.1.0',
     author = "Carson O'Ffill",
     author_email = 'offillcarson@gmail.com',
     license = 'MIT',
